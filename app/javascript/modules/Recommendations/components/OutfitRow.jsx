15024e4c2c2114a45b81ed9cfc1586c2d7857a45
Ali Al-Sheiba <ali.alsheiba@gmail.com>
2018-09-28 00:52:18 +0400

    Update outfits recommendations UI

--- app/javascript/modules/Recommendations/components/OutfitRow.jsx
+++ app/javascript/modules/Recommendations/components/OutfitRow.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ItemRow from '../../Items/components/Row';
+import classNames from 'classnames';
+import ItemRow from 'modules/Items/components/Row';
 
 const Row = ({ outfit: { items, ...outfit } }) => (
   <div className="card">
     <div className="card-body">
       <div className="float-md-right text-right">
-        <small className="d-block">
-          {`Total Price: ${outfit.total_price} AED`}
-        </small>
+        <div className="ml-auto text-muted">
+          <small className="text-muted">{outfit.likes}</small>
+          <i className={classNames('p-2', {
+            'fas fa-heart text-danger': outfit.liked,
+            'far fa-heart': !outfit.liked,
+          })}
+          />
+        </div>
       </div>
       <h5 className="card-title">
         <strong>{outfit.name}</strong>
         {' by: '}
         <span className="text-muted">{outfit.user.name}</span>
       </h5>
+      <small className="card-subtitle">
+        {`Total Price: ${outfit.total_price} AED`}
+      </small>
 
       <div className="flex-nowrap flex-row row row-cards o-auto">
         {items.map(i => (
