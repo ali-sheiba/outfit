b022555a064d44c722da7f1cc5e2eb53fd54a3ab
Ali Al-Sheiba <ali.alsheiba@gmail.com>
2018-09-29 10:41:13 +0400

    Change col size of outfit items

--- app/javascript/modules/Outfits/components/OutfitItems.jsx
+++ app/javascript/modules/Outfits/components/OutfitItems.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const OutfitItems = ({ items }) => (
   <div className="row no-gutters bg-gray-dark-lightest justify-content-center">
     {items.map(i => (
-      <div className="col-sm-4" key={`outfit-item-${i.id}`}>
+      <div className="col-4" key={`outfit-item-${i.id}`}>
         <img src={i.image_url} alt={i.name} />
       </div>
     ))}
